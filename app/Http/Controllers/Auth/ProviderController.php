e53b160034b4389409d58ce94f98b707c5309ff3
David Rodriguez <david@enyutech.com>
2024-01-05 23:46:37 -0500

    let the ProviderController actually create the user accounts for social logins

--- app/Http/Controllers/Auth/ProviderController.php
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -3,16 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class ProviderController extends Controller
 {
-    public function redirect($provider)
-    {
-        return Socialite::driver($provider)->redirect();
-    }
-
     public function callback(Request $request, $provider)
     {
         // Check for errors. If there are any, return back to the login page and display them.
@@ -23,15 +20,56 @@ class ProviderController extends Controller
             ]);
         }
 
-        /* Socialite acts weird with LinkedIn as of 2024-01-04. The provider is 'linkedin-openid' but the callback
-        returns 'linkedin'. When this happens, socialite blows up let's just fix it here for now and probably
-        report this as a bug later. */
+        $username = null;
+
         if ($provider === 'linkedin') {
-            $provider = $provider.'-openid';
+            $provider = $provider.'-openid'; // redirect returns 'linkedin' and not 'linkedin-openid' like it should
+            $username = \Str::random(15); // linkedin doesn't have a username, this is required for our codebase
         }
 
-        // Grab the user's information so we can do things with it.
-        $user = Socialite::driver($provider)->user();
-        // TODO: Create the user
+        if ($provider === 'facebook') {
+            $username = \Str::random(15); // facebook doesn't have a username, this is required for our codebase
+        }
+
+        try {
+            $socialUser = Socialite::driver($provider)->user();
+            $user = User::where([
+                'provider' => $provider,
+                'provider_id' => $socialUser->getId()
+            ])->first();
+
+            if (!$user) {
+                $name = explode(' ', $socialUser->getName());
+                $user = User::create([
+                    'username' => $username ?? $socialUser->getNickname(),
+                    'first_name' => $name[0],
+                    'last_name' => $name[1],
+                    'display_name' => $socialUser->getName(),
+                    'email' => $socialUser->getEmail(),
+                    'provider_id' => $socialUser->getId(),
+                    'provider' => $provider,
+                    'provider_token' => $socialUser->token,
+                    'email_verified_at' => now(),
+                ]);
+
+                $user->save();
+            }
+
+            if ($user) {
+                Auth::login($user);
+            } else {
+                if (User::where('email', $socialUser->getEmail())->exists()) {
+                    return redirect('/login')->withErrors(['email' => 'This email uses a different method to login.']);
+                }
+            }
+        } catch (\Exception $e) {
+            return redirect('/login')->withErrors(['email' => 'This email uses a different method to login.']);
+        }
+        return redirect('/');
+    }
+
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
     }
 }
